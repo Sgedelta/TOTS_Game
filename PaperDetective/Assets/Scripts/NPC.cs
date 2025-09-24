552f662bd4b3bd8cd01f1b93447a2d2f0b23e7b4
ccb1060 <ccb1060@rit.edu>
2025-09-23 20:34:35 -0400

    NPCs can now move

--- PaperDetective/Assets/Scripts/NPC.cs
+++ PaperDetective/Assets/Scripts/NPC.cs
@@ -1,20 +1,36 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class NPC : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
 
+    [SerializeField] private float speed;
+    /// <summary>
+    /// The (x,y) coords that the NPC will move to starting at the 0th index
+    /// and ending at the nth index.
+    /// </summary>
+    [SerializeField] private List<Vector2> moveTo;
     
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        if(moveTo.Count > 0)
+        {
+            rb.linearVelocity = (moveTo[0] - rb.position).normalized * speed;
+
+            if ((moveTo[0] - rb.position).magnitude < 1)
+            {
+                moveTo.RemoveAt(0);
+            }
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+
         
     }
 
