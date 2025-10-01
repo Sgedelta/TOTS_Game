48d55ed6e37bdec31af70809f80abd20da9815aa
ThorFrostick <145503610+ThorFrostick@users.noreply.github.com>
2025-10-01 10:08:50 -0400

    Prepared testing for finding file path in a build

--- PaperDetective/Assets/Scripts/FileManager.cs
+++ PaperDetective/Assets/Scripts/FileManager.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 using Yarn.Unity.Editor;
 
 public class FileManager : MonoBehaviour
@@ -8,27 +9,43 @@ public class FileManager : MonoBehaviour
     [SerializeField]
     private TextAsset jsonFile;
 
+    //Create an instance of the JSON data that we will record
     private UserData userData = new UserData();
 
     //Keep track of the time since the game started
     private float time;
 
+    //Get the testing button
+    [SerializeField]
+    private Button button;
+
+
+    private void Start()
+    {
+        button.onClick.AddListener(SaveFile);
+    }
+
 
     // Update is called once per frame
     void Update()
     {
+        //Update the time played each frame
         time += Time.deltaTime;
     }
 
-    private void OnApplicationQuit()
+    private void SaveFile()
     {
         userData.playTime = $"{time}";
 
+        //Convert our user data into JSON format.
+        //Include "true" to make the format readable.
         string jsonData = JsonUtility.ToJson(userData, true);
 
         //string path = "Assets/InternalLogs/playtest_logs_1.json";
 
         Debug.Log(jsonData);
+
+        Debug.Log(Application.persistentDataPath);
 
         //System.IO.File.WriteAllText(path, jsonData);
     }
