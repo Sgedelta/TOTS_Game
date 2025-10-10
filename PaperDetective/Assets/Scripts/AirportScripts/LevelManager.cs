247c06b7887245bfad4b73ee1838027391ceabbf
ThorFrostick <145503610+ThorFrostick@users.noreply.github.com>
2025-10-10 11:09:32 -0400

    Added dialogue controls for the Vending Machine

--- PaperDetective/Assets/Scripts/AirportScripts/LevelManager.cs
+++ PaperDetective/Assets/Scripts/AirportScripts/LevelManager.cs
@@ -9,20 +9,26 @@ public class LevelManager : MonoBehaviour
 
     //Determine if the Player has the dictionary.
     public bool hasDict;
+
+    //Track the amount of currency the player has.
+    private float money;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         //At the start, the Player does not have the dictionary, so they cannot understand people.
         hasDict = false;
+
+        money = 0.00f;
     }
 
     // Update is called once per frame
     void Update()
     {
         //If the Player picks up the dictionary, they can then understand people.
-        //if(Player.inventory.contains(dictionary))
+        //if(Player.inventory.contains(dictionary) && hasDict == false)
         //{
+        //DictionaryGot();
         //hasDict = true;
         //}
 
@@ -32,16 +38,44 @@ public class LevelManager : MonoBehaviour
         {
             DictionaryGot();
         }
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            AddMoney(4.99f);
+        }
+
+        
+        //If the player has enough money to get the dictionary, make sure the Yarn files know.
+        //if(Player.currency >= 4.99)
+        //{
+        //
     }
 
+    /// <summary>
+    /// Send a message to the Yarn scripts that the player has the dictionary.
+    /// </summary>
     [YarnCommand("has_Dict")]
     private void DictionaryGot()
     {
+        //Get the dialogue runner object in the scene.
         DialogueRunner runner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
 
+        //If we have the dialogue runner(if we don't something is wrong), update the global "hasDict" variable to true.
         if(runner != null)
         {
             runner.VariableStorage.SetValue("$hasDict", true);
+        }
+    }
+
+    [YarnCommand("moreMoney")]
+    private void AddMoney(float newMoney)
+    {
+        //Get the dialogue runner object in the scene.
+        DialogueRunner runner = FindObjectOfType<Yarn.Unity.DialogueRunner>();
+
+        //If we have the dialogue runner, increase the currency that yarn has.
+        if (runner != null)
+        {
+            runner.VariableStorage.SetValue("$currency", money + newMoney);
         }
     }
 }
