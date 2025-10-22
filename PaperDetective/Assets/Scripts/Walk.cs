887a2c9a4031f5e91c93cb15ecb17c6504c88cca
ccb1060 <ccb1060@rit.edu>
2025-10-21 20:56:52 -0400

    Drunk NPC can walk

--- PaperDetective/Assets/Scripts/Walk.cs
+++ PaperDetective/Assets/Scripts/Walk.cs
@@ -4,60 +4,17 @@ public class Walk : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
 
-    [SerializeField] private SpriteRenderer sprite;
-    /// <summary>
-    /// How fast the walk animation is
-    /// </summary>
-    [SerializeField] private float spinSpeed;
-
-    /// <summary>
-    /// How far the player will rotate suring one "step" before rotating the other way
-    /// </summary>
-    [SerializeField] private float maxWalkAngle;
-
-    private float spinDirection = 1;
-
-    private float currentAngle;
-
     [SerializeField] private bool playSound;
 
     [SerializeField] private AudioSource stepPlayer;
+
+    [SerializeField] private Animator anim;
+
     // Update is called once per frame
     void Update()
     {
         //If player is moving, do the rotation
-        if (rb.linearVelocity.x != 0 || rb.linearVelocityY != 0)
-        {
-            if (Mathf.Abs(currentAngle) > maxWalkAngle)
-            {
-                spinDirection *= -1;
-                if (playSound)
-                {
-                    stepPlayer.Play();
-                }
-                
-            }
-            currentAngle += spinDirection * spinSpeed * Time.deltaTime * 100;
-            sprite.transform.Rotate(new Vector3(0, 0, spinDirection * spinSpeed * Time.deltaTime * 100));
-        }
-        //Otherwise slowly return the player to the upright position
-        else if (Mathf.Abs(sprite.transform.rotation.z) > spinSpeed)
-        {
-            if (sprite.transform.rotation.z < 0)
-            {
-                currentAngle += spinSpeed * Time.deltaTime * 100;
-                sprite.transform.Rotate(new Vector3(0, 0, spinSpeed * Time.deltaTime * 100));
-            }
-            else
-            {
-                currentAngle -= spinSpeed * Time.deltaTime * 100;
-                sprite.transform.Rotate(new Vector3(0, 0, -spinSpeed * Time.deltaTime * 100));
-            }
-        }
-
-        if(transform.rotation.z != 0)
-        {
-            transform.rotation = Quaternion.identity;
-        }
+        anim.SetFloat("Velocity", rb.linearVelocity.magnitude);
+        anim.SetFloat("HorizontalVelocity", rb.linearVelocity.x);
     }
 }
