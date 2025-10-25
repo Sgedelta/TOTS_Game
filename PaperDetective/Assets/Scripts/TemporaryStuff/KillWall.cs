7f74045e56ca76a990801d4035b29f3f77d9f783
ThorFrostick <145503610+ThorFrostick@users.noreply.github.com>
2025-10-25 17:30:01 -0400

    Added dialogue to block player from leaving early

--- PaperDetective/Assets/Scripts/TemporaryStuff/KillWall.cs
+++ PaperDetective/Assets/Scripts/TemporaryStuff/KillWall.cs
@@ -5,17 +5,30 @@ public class KillWall : MonoBehaviour
 {
     [SerializeField]
     private GameObject wall;
+
+    private DialogueRunner runner;
+
+    [SerializeField]
+    private string node;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        runner = FindFirstObjectByType<DialogueRunner>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 6)
+        {
+            runner.StartDialogue(node);
+        }
     }
 
     [YarnCommand("KillWall")]
