4cec9224376d774ea79824ad26747c670b1e24b4
Sgedelta <Sgeaston04@gmail.com>
2025-11-10 17:49:45 -0500

    small addition to player position persistence, two methods to do persistent position now - player and everything else

--- PaperDetective/Assets/Scripts/TriggerSceneChange.cs
+++ PaperDetective/Assets/Scripts/TriggerSceneChange.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 public class TriggerSceneChange : MonoBehaviour
 {
     [SerializeField] private string sceneName;
+    
+    //these don't *have* to be unique, but they need to be unique to a scene (the one you are entering into)
+    [SerializeField] private string entranceName;
 
     private sceneManager sceneManager;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -15,6 +18,14 @@ public class TriggerSceneChange : MonoBehaviour
     {
         if (collision.gameObject.GetComponent<PLayerController>())
         {
+            //get the player's persistent data - will need an idField and more logic to do any item, for now only setting player locations
+            //  if this isn't working, check the Persistent Data Checker ID on the player is set correctly
+            GameManager.instance.AllPersistentData[collision.gameObject.GetComponent<PersistentDataChecker>().ObjectID].CurrentObjectState =
+                entranceName; // note: Update if player object state ever gets more complex...
+
+
+
+            //load the next scene
             sceneManager.ChangeScene(sceneName);
         }
     }
